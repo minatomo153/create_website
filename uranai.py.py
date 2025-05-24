7e02e0719464f1b840e8178c0ed30daa2911bfea
minatomo-admin <zat12472@gmail.com>
2025-05-24 15:40:16 +0900

    占い内容を修正

--- uranai.py.py
+++ uranai.py.py
@@ -1,23 +1,26 @@
 import random
 import streamlit as st
 import pandas as pd
-
+import time
 
 st.title("今日の運勢を占いましょう")
 kekka = ""
+dice_sum = 0
 
-if st.button("運勢を占う"):
-    dice1 = random.randint(1, 50)  # 1-6までをランダムに生成
-    dice2 = random.randint(1, 50)
-    dice_sum = dice1 + dice2  # 2つのサイコロの値を合計
+if st.button("本日の運勢"):
+    uranaiDice1 = random.randint(1, 10)  # 1-6までをランダムに生成
+    uranaiDice2 = random.randint(1, 10)
+    uranaiDice_sum = uranaiDice1 + uranaiDice2  # 2つの値を合計
+    with st.spinner('占い中.'):
+        time.sleep(3)	
    
-    if dice_sum == 1:
-        kekka = "大吉"
-    if dice_sum >= 2 & dice_sum <= 49:
-        kekka = "小吉"
-    if dice_sum >= 50 & dice_sum <= 99:
-        kekka = "中吉"
-    if dice_sum == 100:
+    if uranaiDice_sum == 1:
         kekka = "大大吉"
+    elif uranaiDice_sum >=2  and uranaiDice_sum <= 9:
+        kekka = "小吉"
+    elif uranaiDice_sum >= 10 and uranaiDice_sum <= 16:
+        kekka = "中吉"
+    else:
+        kekka = "大吉"
 
 st.write("占い結果: ", kekka)
